02001485c9216764dad54c77b0ce2fbce8c5e42f
millerdylan12 <dylan@jkmventures.net>
2025-04-29 11:25:28 -0500

    chore: restore Terraform HCL and add CI trigger comment

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,50 +1,104 @@
-# ci-trigger-line
+# ci-trigger
 
-name: Terraform CI/CD
+// ----------------------------------------
+// infra/terraform/main.tf
+// ----------------------------------------
 
-on:
-  push:
-    paths:
-      - 'infra/terraform/**'
-  pull_request:
-    paths:
-      - 'infra/terraform/**'
-  workflow_dispatch:
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
 
-permissions:
-  id-token: write
-  contents: read
+provider "aws" {
+  region = "us-east-1"
+}
 
-jobs:
-  terraform:
-    runs-on: ubuntu-latest
+data "aws_availability_zones" "available" {}
 
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v3
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-      - name: Configure AWS Credentials via OIDC
-        uses: aws-actions/configure-aws-credentials@v3
-        with:
-          role-to-assume: ${{ secrets.ACTIONS_ROLE_ARN }}
-          aws-region:     us-east-1
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
 
-      - name: Setup Terraform
-        uses: hashicorp/setup-terraform@v2
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
 
-      - name: Terraform Init
-        working-directory: infra/terraform
-        run: terraform init -input=false
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
 
-      - name: Terraform Plan
-        working-directory: infra/terraform
-        run: terraform plan -input=false
+  name = "swing-trade-vpc"
+  cidr = var.vpc_cidr
 
-      - name: Terraform Apply
-        if: github.ref == 'refs/heads/main' && github.event_name == 'push'
-        working-directory: infra/terraform
-        run: terraform apply -input=false -auto-approve
+  azs             = data.aws_availability_zones.available.names[0:3]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 
+  enable_nat_gateway = true
+  single_nat_gateway = false
+}
 
+data "aws_iam_policy_document" "github_oidc_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:millerdylan12/Swing-Trading-Platform:ref:refs/heads/main"]
+    }
+  }
+}
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
 
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-deploy-role"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_iam" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  description = "ARN of the OIDC role for GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
